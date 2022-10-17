7cb6e9f1880e57c19179c0a4b7e2e8cdeb17ad45
dharani-CK <113404041+dharani-CK@users.noreply.github.com>
2022-10-17 17:49:01 -0500

    Create Exercise#2.13.java

---
+++ Exercise#2.13.java
@@ -0,0 +1,31 @@
+/* This program prompts the user to enter a monthly saving amount and dis￾plays the account value after the sixth month. */
+
+import java.util.Scanner; //creating a scanner class
+
+public class MonthlyInterest
+{
+	public static void main(String[] args) {
+    
+		Scanner input = new Scanner(System.in); //creating a new scanner class object
+		
+		//prompt the user to enter the monthly saving
+		System.out.print("Enter the monthly saving amount: $");
+		double savingAmount = input.nextDouble();
+		
+		double annualInterestRate = 0.05; //annual interest rate
+		double monthlyInterestRate = annualInterestRate / 12; //calculating the monthly interest rate
+		
+    //calculating the amount for every month
+		double firstMonthAmount = savingAmount * (1 + monthlyInterestRate);
+		double secondMonthAmount = (savingAmount + firstMonthAmount) * (1 + monthlyInterestRate);
+		double thirdMonthAmount = (savingAmount + secondMonthAmount) * (1 + monthlyInterestRate);
+		double fourthMonthAmount = (savingAmount + thirdMonthAmount) * (1 + monthlyInterestRate);
+		double fifthMonthAmount = (savingAmount + fourthMonthAmount) * (1 + monthlyInterestRate);
+		double sixthMonthAmount = (savingAmount + fifthMonthAmount) * (1 + monthlyInterestRate);
+		
+		
+		//display the results
+		System.out.println("After the sixth month, the account value is $" + sixthMonthAmount);
+		
+	}
+}
